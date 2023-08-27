4464aa70daa9ebf0f6dc689e824e2e7c8470f73b
SYDMAZHAR <142782345+SYDMAZHAR@users.noreply.github.com>
2023-08-27 14:45:41 +0530

    Update python assessment2.py

--- python assessment2.py
+++ python assessment2.py
@@ -1,18 +1,18 @@
-def sort_key(tuple_item):
-    return tuple_item[-1]
+# def sort_key(tuple_item):
+#     return tuple_item[-1]
 
-def sort_tuples_by_last_element(tuple_list):
-    sorted_list = sorted(tuple_list, key=sort_key)
-    return sorted_list
+# def sort_tuples_by_last_element(tuple_list):
+#     sorted_list = sorted(tuple_list, key=sort_key)
+#     return sorted_list
 
-# Sample List
-sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
+# # Sample List
+# sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 
-# Sorting the list
-result = sort_tuples_by_last_element(sample_list)
+# # Sorting the list
+# result = sort_tuples_by_last_element(sample_list)
 
-# Printing the sorted result
-print("Expected Result:", result)
+# # Printing the sorted result
+# print("Expected Result:", result)
 
 
 
