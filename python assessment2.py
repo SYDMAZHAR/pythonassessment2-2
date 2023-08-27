8abf4e9b2ca488215b9b8c8edabb15e9d2683281
SYDMAZHAR <142782345+SYDMAZHAR@users.noreply.github.com>
2023-08-27 14:47:18 +0530

    Update python assessment2.py

--- python assessment2.py
+++ python assessment2.py
@@ -1,20 +1,3 @@
-# def sort_key(tuple_item):
-#     return tuple_item[-1]
-
-# def sort_tuples_by_last_element(tuple_list):
-#     sorted_list = sorted(tuple_list, key=sort_key)
-#     return sorted_list
-
-# # Sample List
-# sample_list = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
-
-# # Sorting the list
-# result = sort_tuples_by_last_element(sample_list)
-
-# # Printing the sorted result
-# print("Expected Result:", result)
-
-
 
 ascii_dict = {}
 for char in range(ord('a'), ord('z') + 1):
